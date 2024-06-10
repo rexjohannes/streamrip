45605f652728b15e899ec0d9a920afeba94d5666
rexjohannes <53578137+rexjohannes@users.noreply.github.com>
2024-06-10 17:22:54 +0000

    handle umlauts

--- streamrip/filepath_utils.py
+++ streamrip/filepath_utils.py
@@ -5,8 +5,13 @@ from pathvalidate import sanitize_filename, sanitize_filepath  # type: ignore
 ALLOWED_CHARS = set(printable)
 
 
+def replace_umlauts(s):
+    return s.replace("Ä", "Ae").replace("ä", "ae").replace("Ö", "Oe").replace("ö", "oe").replace("Ü", "Ue").replace("ü", "ue").replace("ß", "ss")
+
+
 def clean_filename(fn: str, restrict: bool = False) -> str:
     path = str(sanitize_filename(fn))
+    path = replace_umlauts(path)
     if restrict:
         path = "".join(c for c in path if c in ALLOWED_CHARS)
 
@@ -15,6 +20,7 @@ def clean_filename(fn: str, restrict: bool = False) -> str:
 
 def clean_filepath(fn: str, restrict: bool = False) -> str:
     path = str(sanitize_filepath(fn))
+    path = replace_umlauts(path)
     if restrict:
         path = "".join(c for c in path if c in ALLOWED_CHARS)
 
